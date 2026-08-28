ee5c8139c2dc3a69f716f886d1b464f056216a73
khamphouphimmasone176-sudo <khamphouphimmasone176@gmail.com>
2026-08-29 01:29:27 +0700

    Handle unsupported maps with a print statement
    Added a print statement for unsupported maps.

--- loader.lua
+++ loader.lua
@@ -1,13 +1,11 @@
 local PlaceId = game.PlaceId
 
 local MapScripts = {
-    [107778070777162] = "https://raw.githubusercontent.com/khamphouphimmasone176-sudo/-/refs/heads/main/steal.lua",   -- ถ้าอยู่แมพนี้ มันจะไปดึงโค้ดในไฟล์ farm.lua มาเปิด
-    [2222222]  = "ลิงก์_Raw_ของไฟล์_steal.lua",  -- ถ้าอยู่แมพนี้ มันจะไปดึงโค้ดในไฟล์ steal.lua มาเปิด
+    [107778070777162] = "https://raw.githubusercontent.com/khamphouphimmasone176-sudo/-/refs/heads/main/steal.lua",
 }
 
 if MapScripts[PlaceId] then
     loadstring(game:HttpGet(MapScripts[PlaceId]))()
 else
-    -- ถ้าไปเล่นแมพอื่นที่ไม่ได้ใส่ไว้ในตารางด้านบน มันจะมาเปิดไฟล์หลักอันนี้แทน
-    loadstring(game:HttpGet("ลิงก์_Raw_ของไฟล์_main.lua"))()
+    print("แมพนี้ยังไม่ได้ใส่สคริปต์")
 end
