e4d855a3740070840951ac670667d6d7adadc67c
khamphouphimmasone176-sudo <khamphouphimmasone176@gmail.com>
2026-08-29 01:18:43 +0700

    Update MapScripts with new URL for specific PlaceId

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local PlaceId = game.PlaceId
 
 local MapScripts = {
-    [11111111] = "ลิงก์_Raw_ของไฟล์_farm.lua",   -- ถ้าอยู่แมพนี้ มันจะไปดึงโค้ดในไฟล์ farm.lua มาเปิด
+    [107778070777162] = "https://raw.githubusercontent.com/khamphouphimmasone176-sudo/-/refs/heads/main/steal.lua",   -- ถ้าอยู่แมพนี้ มันจะไปดึงโค้ดในไฟล์ farm.lua มาเปิด
     [2222222]  = "ลิงก์_Raw_ของไฟล์_steal.lua",  -- ถ้าอยู่แมพนี้ มันจะไปดึงโค้ดในไฟล์ steal.lua มาเปิด
 }
 
